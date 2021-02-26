2385f889173439e0837719b386e3b8963bc2036e
Halle Helfrich <hallehelfrich@dhcp-172-16-236-237.dyn.pittstate.edu>
2021-02-26 08:31:51 -0600

    update

--- testDerivatives.py
+++ testDerivatives.py
@@ -1,7 +1,17 @@
-F(x) = 0
-f(x) = 0
-def testA(x):
-    return F(x) = 49*x**6 + 48*x**5 + 3*x**4 + 39*x**2 +23*x +35, f(x) = 294*x**5 + 240*x**4 + 14*x**3 + 78**x
+import numericalDeriv
 
-import numericalDeriv.py
+h = .01
+x = 0
 
+def f(x):
+    f = 49*x**6 + 48*x**5 + 3*x**4 + 39*x**2 +23*x + 35
+    return f
+
+def forwardF(f, x, h):
+    hand_x = 294*x**5 + 240*x**4 + 14*x**3 + 78**x
+    forward_x = numericalDeriv.forwardDiff(f, x, h)
+    return f, hand_x, forward_x
+
+f, hand_x, forward_x = forwardF(f, x, h)
+
+print(f, " ", hand_x, " ", forward_x)
